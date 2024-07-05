e6c6bd4ec1f8de52ece2029d64c134d579a9c662
mark0101 <m_ark.niemeijer@hotmail.com>
2024-07-05 13:44:58 +0200

    Added missing reference to regex class

--- No u discord bot/Commands/DiceRollCommands.cs
+++ No u discord bot/Commands/DiceRollCommands.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace No_u_discord_bot.Commands
